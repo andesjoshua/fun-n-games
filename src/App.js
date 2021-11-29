38df160aa7eeeb824835650b8e79962d519b147a
Joshua Andes <andesjoshua@comcast.net>
2021-11-29 13:28:59 -0500

    load screen

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import './App.css';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
+import {useEffect, useState} from 'react'
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <div className="home">
+      {loading ? "loading..." : "hello"}
       <section className='header'>
         <Header />
       </section>
