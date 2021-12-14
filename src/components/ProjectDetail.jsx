7936771b59e4945bc22c0067e2d10d48c06799bc
Joshua Andes <andesjoshua@comcast.net>
2021-12-14 13:20:29 -0500

    added new projects

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -7,8 +7,8 @@ const ProjectDetail = ({project: {contributors, title, technologies, link}}) => 
    return (
        <div className='individual-project'>
            <h1 style={{fontSize: 30}}>{title}</h1>
-           <p>This was a group project with {contributors} contributors, myself and {contributors - 1} other(s).</p>
-           <p>Technolgies used in this project include: {tech}</p>
+           {contributors === '1' ? <p>This was a solo project</p> : <p>This was a group project with {contributors} contributors, myself and {contributors - 1} other(s).</p>}
+           <p>Technolgies include: {tech}</p>
            <a href={link} target='_blank'>{link ? `${title}` : "Coming soon"} </a>
        </div>
    )
--- src/components/projectData.js
+++ src/components/projectData.js
@@ -3,9 +3,23 @@ import React from "react";
 export const projectData = [
     {
         id: 1,
+        title: "Metabook",
+        technologies: ["React.js, ", "CSS, ", "thirdweb API, ", "and Ruby on Rails."],
+        contributors: "1",
+    },
+    {
+        id: 3,
         title: "Fun N' Games",
         technologies: ["React.js, ", "CSS, ", "and Vanilla JS."],
         contributors: "2",
         link: "https://game-search-engine.herokuapp.com/"
     },
+    {
+        id: 2,
+        title: "Mount Algo",
+        technologies: ["React.js, ", "CSS, ", "Ruby on Rails, ", "and Vanilla JS."],
+        contributors: "3",
+        link: "https://still-garden-51529.herokuapp.com/"
+    }
+   
 ]
